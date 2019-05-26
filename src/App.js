0cae54aa87d5392774422f06982c2df0572d22ac
zanenkn <zane.neikena@gmail.com>
2019-05-26 21:46:34 +0200

    Changes the state via store.dispatch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import {Container, Header} from 'semantic-ui-react'
+import {Container, Header, Button} from 'semantic-ui-react'
 import { connect } from 'react-redux';
 
 const App = (props) => {
   return (
     <Container>
       <Header as="h1">{props.state.greeting}</Header>
+      <Button
+          primary
+          onClick={() => props.dispatch({ type: 'CHANGE_GREETING', greeting: 'Yo World, Wazzup' })}
+      >
+        Change greeting
+      </Button>
     </Container>
   )
 }
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,13 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
-  return state
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_GREETING') {
+    return {
+      ...state,
+      greeting: action.greeting
+    }
+  } else {
+    return state
+  }
 }
-
 export default rootReducer
