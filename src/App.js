7635e5284b0c5480c464a3037e2aafc81d3b7f15
zanenkn <zane.neikena@gmail.com>
2019-05-26 21:35:10 +0200

    Connects the App component to Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import {Container, Header} from 'semantic-ui-react'
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
   return (
     <Container>
-      <Header as="h1">Hello World</Header>
+      <Header as="h1">{props.state.greeting}</Header>
     </Container>
   )
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    state: state
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(App)
