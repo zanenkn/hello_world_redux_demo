7ab22f84e6a9b3c205d8b916fc0f549093fa6705
zanenkn <zane.neikena@gmail.com>
2019-05-26 22:07:05 +0200

    refactoring

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,18 +1,20 @@
 import initialState from '../store/initialState'
 
 const rootReducer = (state = initialState, action) => {
-  if (action.type === 'CHANGE_GREETING') {
-    return {
-      ...state,
-      greeting: state.proposed_greeting
-    }
-  } else if (action.type === 'PROPOSE_GREETING') {
-    return {
-      ...state,
-      proposed_greeting: action.greeting
-    }
-  } else {
-    return state
+  switch (action.type) {
+    case 'CHANGE_GREETING':
+      return {
+        ...state,
+        greeting: state.proposed_greeting
+      }
+    case 'PROPOSE_GREETING':
+      return {
+        ...state,
+        proposed_greeting: action.greeting
+      }
+    default:
+      return state
   }
 }
+
 export default rootReducer
